a6af14b6f32ceca43d736c006a16ab13b7f4c085
blademainer <blademainer@gmail.com>
2020-10-07 16:43:42 +0800

    update http

--- tracinghttp/client.go
+++ tracinghttp/client.go
@@ -2,10 +2,6 @@ package tracinghttp
 
 import (
 	"context"
-	"github.com/opentracing/opentracing-go"
-	"github.com/opentracing/opentracing-go/ext"
-	tracinglogger "github.com/pjoc-team/tracing/logger"
-	"github.com/pjoc-team/tracing/util"
 	"io"
 	"net/http"
 )
@@ -27,12 +23,10 @@ func Post(ctx context.Context, c *http.Client, url, contentType string, body io.
 }
 
 func do(ctx context.Context, c *http.Client, url, method, contentType string, header map[string]string, body io.Reader) (spanCtx context.Context, resp *http.Response, err error) {
-	uri := util.GetPath(url)
-	span, spanCtx := opentracing.StartSpanFromContext(ctx, uri)
-	defer span.Finish()
+	client := NewClient(c)
 	req, err := http.NewRequest(method, url, body)
 	if err != nil {
-		return spanCtx, nil, err
+		return ctx, nil, err
 	}
 	if contentType != "" {
 		req.Header.Set("Content-Type", contentType)
@@ -42,23 +36,6 @@ func do(ctx context.Context, c *http.Client, url, method, contentType string, he
 			req.Header.Set(k, v)
 		}
 	}
-	ext.SpanKindRPCClient.Set(span)
-	ext.HTTPUrl.Set(span, url)
-	ext.HTTPMethod.Set(span, method)
-	err = span.Tracer().Inject(
-		span.Context(),
-		opentracing.HTTPHeaders,
-		opentracing.HTTPHeadersCarrier(req.Header),
-	)
-	if err != nil {
-		tracinglogger.ContextLog(spanCtx).Errorf("inject to HTTPHeaders err %v", err)
-	}
-	resp, err = c.Do(req)
-	if err != nil {
-		ext.Error.Set(span, true)
-		return spanCtx, resp, err
-	} else {
-		ext.HTTPStatusCode.Set(span, uint16(resp.StatusCode))
-	}
-	return spanCtx, resp, nil
+	resp, err = client.Do(req)
+	return ctx, resp, err
 }
